fb22a9a8fdd57908436577c0643c0e2d07b7e0d0
squigglybob <squigglybob@users.noreply.github.com>
2022-08-04 11:30:38 +0100

    feat: use the new wizard filter in the prayer campaigns plugin (#4)
    * feat: use the new wizard filter in the prayer campaigns plugin
    
    * refactor: use porch id as wizard id

--- porch/dt-110-cities-porch-loader.php
+++ porch/dt-110-cities-porch-loader.php
@@ -8,6 +8,18 @@ class DT_110_Cities_Porch_Loader extends DT_Generic_Porch_Loader {
         parent::__construct( __DIR__ );
 
         $this->label = __( '110 Cities Porch', 'disciple-tools-prayer-campaign' );
+        add_filter( 'dt_campaigns_wizard_types', array( $this, 'wizard_types' ) );
+    }
+
+    public function wizard_types( $wizard_types ) {
+        $hundred_ten_wizard = [
+            "campaign_type" => "ongoing",
+            "porch" => $this->id,
+            "label" => "Setup Landing page for 110 Cities Campaign",
+        ];
+        $wizard_types[$this->id] = $hundred_ten_wizard;
+
+        return $wizard_types;
     }
 }
 ( new DT_110_Cities_Porch_Loader() )->register_porch();
